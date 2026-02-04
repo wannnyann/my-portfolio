74441354ddb229d192b925f7e781ab567b3bd8c9
wannnyann <takerun.rikkun@gmail.com>
2026-02-04 18:20:51 +0900

    chore: ignore Next.js build output (.next)

--- my-app/.next/dev/static/chunks/study_my-portfolio_my-app_app_admin_page_tsx_5fc75a13._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/ef895_bf4abf69._.js",
-  "static/chunks/study_my-portfolio_my-app_b23ee9fd._.js"
-],
-    source: "dynamic"
-});
